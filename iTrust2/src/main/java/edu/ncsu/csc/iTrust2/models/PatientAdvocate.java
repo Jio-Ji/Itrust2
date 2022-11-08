512af310e1fb531e47f0e8d9b9a166266c7033f1
abheywar <abheywar@ncsu.edu>
2022-11-08 15:29:34 -0500

    Completed PatientAdvocate and created associated PatientAdvocateForm

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/models/PatientAdvocate.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/models/PatientAdvocate.java
@@ -3,7 +3,9 @@ package edu.ncsu.csc.iTrust2.models;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
 
+import org.hibernate.mapping.List;
 import org.hibernate.validator.constraints.Length;
 
 import edu.ncsu.csc.iTrust2.forms.PatientAdvocateForm;
@@ -88,6 +90,12 @@ public class PatientAdvocate extends User {
     private String zip;
 
     /**
+     * Patients associated with the Patient Advocate
+     */
+    @OneToMany
+    private List   patients;
+
+    /**
      * For Hibernate
      */
     public PatientAdvocate () {
@@ -104,7 +112,7 @@ public class PatientAdvocate extends User {
         super( uf );
         if ( !getRoles().contains( Role.ROLE_PA ) ) {
             throw new IllegalArgumentException(
-                    "Attempted to create a Patient Advocate record for a non-Patient user!" );
+                    "Attempted to create a Patient Advocate record for a non-Patient Advocate user!" );
         }
     }
 
@@ -116,6 +124,116 @@ public class PatientAdvocate extends User {
      * @return updated PatientAdvocate
      */
     public PatientAdvocate update ( final PatientAdvocateForm form ) {
+        setFirstName( form.getFirstName() );
+        setPreferredName( form.getPreferredName() );
+        setLastName( form.getLastName() );
+        setEmail( form.getEmail() );
+        setPhone( form.getPhone() );
+        setAddress1( form.getAddress1() );
+        setAddress2( form.getAddress2() );
+        setCity( form.getCity() );
+        setState( State.parse( form.getState() ) );
+        setZip( form.getZip() );
 
+        setPatients( form.getPatients() );
+
+        return this;
     }
+
+    public String getFirstName () {
+        return firstName;
+    }
+
+    public void setFirstName ( final String firstName ) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName () {
+        return middleName;
+    }
+
+    public void setMiddleName ( final String middleName ) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName () {
+        return lastName;
+    }
+
+    public void setLastName ( final String lastName ) {
+        this.lastName = lastName;
+    }
+
+    public String getPreferredName () {
+        return preferredName;
+    }
+
+    public void setPreferredName ( final String preferredName ) {
+        this.preferredName = preferredName;
+    }
+
+    public String getEmail () {
+        return email;
+    }
+
+    public void setEmail ( final String email ) {
+        this.email = email;
+    }
+
+    public String getPhone () {
+        return phone;
+    }
+
+    public void setPhone ( final String phone ) {
+        this.phone = phone;
+    }
+
+    public String getAddress1 () {
+        return address1;
+    }
+
+    public void setAddress1 ( final String address1 ) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2 () {
+        return address2;
+    }
+
+    public void setAddress2 ( final String address2 ) {
+        this.address2 = address2;
+    }
+
+    public String getCity () {
+        return city;
+    }
+
+    public void setCity ( final String city ) {
+        this.city = city;
+    }
+
+    public State getState () {
+        return state;
+    }
+
+    public void setState ( final State state ) {
+        this.state = state;
+    }
+
+    public String getZip () {
+        return zip;
+    }
+
+    public void setZip ( final String zip ) {
+        this.zip = zip;
+    }
+
+    public List getPatients () {
+        return patients;
+    }
+
+    public void setPatients ( final List patients ) {
+        this.patients = patients;
+    }
+
 }
